fe9424ff1bd10400ca72228f57e015b49cb3c463
Xavih830 <120606644+Xavih830@users.noreply.github.com>
2024-01-05 16:37:40 -0500

    Update inventario.sql

--- inventario.sql
+++ inventario.sql
@@ -20,6 +20,8 @@ CREATE TABLE IF NOT EXISTS EMPLEADO (
   APELLIDOS VARCHAR(255) NOT NULL,
   CARGO ENUM("Asistente de Facturación", "Asistente de Cobranzas", "Asistente de Contabilidad", "Asistente de Importaciones", "Bodega", "Compras", "Desarrollador", "Implementador", "Gerente de Operaciones", "Gerente de Ventas", "Gerente General", "Pasante", "Recepcionista", "Supervisor", "Recursos Humanos", "Técnico"),
   DEPARTAMENTO INT,
+  ES_ADMIN BOOLEAN,
+  ES_USUARIO BOOLEAN,
   AREA INT,
   EXT_TLF INT,
   FOREIGN KEY (DEPARTAMENTO, AREA) REFERENCES DEPARTAMENTO (ID, AREA)
